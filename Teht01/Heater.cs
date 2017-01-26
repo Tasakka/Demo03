c29fc229c113c90250d8b2ba4f7d71b5e59db23b
k3236 <k3236@student.jamk.fi>
2017-01-26 10:56:53 +0200

    jee

---
+++ Teht01/Heater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Heater
+    {
+        // properties
+        public bool IsOn { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        // constructor
+
+        // methods
+        public void PrintData()
+        {
+            Console.WriteLine("How warm is it: " + Temperature);
+            Console.WriteLine("What is the humidity: " + Humidity);
+        }
+        public void Power(bool value)
+        {
+            IsOn = value;
+            Console.WriteLine("Is the stove on: " + value);
+        }
+
+        // deastructor
+        ~Heater()
+        {
+            Console.WriteLine("Heater object detroyed!");
+        }
+    }
+}
---
+++ Teht01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one instance of heater class
+            Heater heater = new Teht01.Heater();
+            heater.PrintData();
+            heater.Power(true);
+            heater.Temperature = 80.6;
+            heater.Humidity = 98.2;
+            heater.PrintData();
+            // testing...
+        }
+    }
+}
