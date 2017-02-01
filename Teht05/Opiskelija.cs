9059d09db38208eb39b1382f0ef2c948aff94171
k3236 <k3236@student.jamk.fi>
2017-02-01 15:01:56 +0200

    hg

---
+++ Teht05/Opiskelija.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Opiskelija
+    {
+        public string Numero { get; set; }
+        public string Nimi { get; set; }
+        public string Luokka { get; set; }
+        public int Ikä { get; set; }
+        public void Name(string name)
+        {
+            Nimi = name;
+        }
+        public void Number(string number)
+        {
+            Numero = number;
+        }
+        public void Age(string age)
+        {
+            Ikä = age;
+        }
+        public void Class(string luokka)
+        {
+            Luokka = luokka;
+        } 
+
+    }
+}
---
+++ Teht05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija = new Opiskelija();
+            string[] opiskelijat = new string[5];
+            opiskelijat[0] = /*opiskelija.Name("moi"); + opiskelija.Number("K3236") + */opiskelija.Age("20")/* + opiskelija.Class("TTV15S1")*/;
+            opiskelijat[1] = opiskelija.Name("Maija") + opiskelija.Number("K1144") + opiskelija.Age(25) + opiskelija.Class("TTV16S3");
+            opiskelijat[2] = opiskelija.Name("Kaapo") + opiskelija.Number("K9654") + opiskelija.Age(25) + opiskelija.Class("TTV15S3");
+            opiskelijat[3] = opiskelija.Name("Mauri") + opiskelija.Number("K1134") + opiskelija.Age(93) + opiskelija.Class("TTV95S3");
+            opiskelijat[4] = opiskelija.Name("Pekka") + opiskelija.Number("K3456") + opiskelija.Age(22) + opiskelija.Class("TTV14S3");
+            for (int i = 0; i < opiskelijat.Length; i++) Console.Write(opiskelijat[i]);
+        }
+    }
+}
